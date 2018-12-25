c9e48f48d24d5f2459a348132c3ae50aedfdae98
jlang2 <jlang2@ncsu.edu>
2018-12-25 15:26:56 -0500

    added smooth scroll

--- shift_code.js
+++ shift_code.js
@@ -1,4 +1,5 @@
+// Changes the class of an element. Used in map hover states.
 function shift(id, newClass) {
-   identity=document.getElementById(id);
-   identity.className=newClass;
+  identity = document.getElementById(id);
+  identity.className = newClass;
 }
